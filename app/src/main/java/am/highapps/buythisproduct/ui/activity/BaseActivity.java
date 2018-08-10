4fa471c9ed194a76b33fb0d8a2be13e4a09d66ab
Khach <khachatryan.khach@gmail.com>
2018-08-10 21:36:40 +0400

    [+] create ui package-

---
+++ app/src/main/java/am/highapps/buythisproduct/ui/activity/BaseActivity.java
@@ -0,0 +1,11 @@
+package am.highapps.buythisproduct.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Khachik Khachatryan on 10.08.2018.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+}
--- app/src/main/java/am/highapps/buythisproduct/ui/activity/MainActivity.java
+++ app/src/main/java/am/highapps/buythisproduct/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package am.highapps.buythisproduct;
+package am.highapps.buythisproduct.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import am.highapps.buythisproduct.R;
 
 public class MainActivity extends AppCompatActivity {
 
